8d0b99c51b9211874d1cfc568da8bd44e8fc2fc7
Gustavo Souza <gustavodonizetesouza@gmail.com>
2019-04-28 17:13:35 -0300

    lazy loading

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import { AboutPage } from '../about/about';
+import { NavController, IonicPage } from 'ionic-angular';
 import { TabsPage } from '../tabs/tabs';
 
+@IonicPage()
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html'  //referencia para qual arquivo é controlado por este controlador
 })
 export class HomePage {
 
